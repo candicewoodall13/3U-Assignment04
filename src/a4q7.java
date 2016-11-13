06065c132a8f7b631f981409ee8f443ba12b260a
Candice <Candice@192.168.0.22>
2016-11-13 17:52:39 -0500

    assignment 4 a4q8 update

--- src/a4q7.java
+++ src/a4q7.java
@@ -24,6 +24,33 @@ public class a4q7 {
         int limit = input.nextInt();
         System.out.println("Enter the recorded speed of the car:");
         int speed = input.nextInt();
+        //calculate fine
+        int excess = speed-limit;
+        //if statement to determie fine
+        if(excess<0)
+        {
+          System.out.println("Congratulations, you are within the speed limit!");
+        } 
+        else if(excess<21)
+          {
+                  System.out.println("You are speeding and your fine is $100");
+          }
+          else if(excess<31)
+                  {
+                   System.out.println("You are speeding and your fine is $270");
+                  }
+                   else
+                      {
+                      System.out.println("You are speeding and your fine is $500");
+                      }
+                      
+                  
+        
+        
+        
+
+              
+        
         
         
     }
--- src/a4q8.java
+++ src/a4q8.java
@@ -1,3 +1,6 @@
+
+import java.util.Scanner;
+
 /*
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
@@ -13,6 +16,30 @@ public class a4q8 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        //create input for the user
+        Scanner input= new Scanner(System.in);
+        //create variables for square and roll
+        int square = 1;
+        int roll= 0;
+        
+        
+        //loop forever
+        while(square<101)
+        {
+            
+        //ask user for sum of dice
+        System.out.println("Enter sum of dice ");
+        //get input from user
+        roll = input.nextInt();
+        //move to the right square
+        square= square+roll;
+
+            
+            /*switch(square){
+                case: 29;
+                    square=34
+                            */
+            System.out.println("You are now on square " +square);
+        }
     }
 }
